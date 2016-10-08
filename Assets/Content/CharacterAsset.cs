141523b8037bc7b807644d1acc141b49cc4b0746
MW <N/A>
2016-10-08 11:45:01 +0200

    add character asset stub

---
+++ Assets/Content/CharacterAsset.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CharacterAsset : IAsset
+{
+    private AssetInfo metaData = null;
+    private GameObject gameObject = null;
+
+    public CharacterAsset(AssetInfo assetInfo, GameObject go)
+    {
+        metaData = assetInfo;
+        gameObject = go;
+    }
+
+    public AssetInfo AssetInfo
+    {
+        get 
+        { 
+            return metaData; 
+        }
+    }
+
+    public T TryGetAsType<T>()
+    {
+        if(gameObject.GetType() == typeof(T))
+        {
+            return (T)Convert.ChangeType(gameObject, typeof(T));
+        }
+        return default(T);
+    }
+
+    public void Unload()
+    {
+        if(gameObject != null)
+        {
+            GameObject.Destroy(gameObject);
+            gameObject = null;
+        }
+    }
+}
