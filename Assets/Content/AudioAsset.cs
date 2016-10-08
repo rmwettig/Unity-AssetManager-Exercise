8417cf665cafae05b865de5c6e95bdeeaa15c517
MW <N/A>
2016-10-08 12:15:57 +0200

    add audio asset class

---
+++ Assets/Content/AudioAsset.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AudioAsset : IAsset
+{
+    private AssetInfo metaData = null;
+    private AudioClip audioClip = null;
+
+    public AudioAsset(AssetInfo assetInfo, AudioClip clip)
+    {
+        metaData = assetInfo;
+        audioClip = clip;
+    }
+
+    public AssetInfo AssetInfo
+    {
+        get
+        {
+            return metaData;
+        }
+    }
+
+    public T TryGetAsType<T>()
+    {
+        if (audioClip.GetType() == typeof(T))
+        {
+            return (T)Convert.ChangeType(audioClip, typeof(T));
+        }
+        return default(T);
+    }
+
+    public void Unload()
+    {
+        if (audioClip != null)
+        {
+            //null only if unloading was successful
+            if (audioClip.UnloadAudioData())
+            {
+                audioClip = null;
+            }
+        }
+    }
+}
