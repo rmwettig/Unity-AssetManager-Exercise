95473e32f3437a232fd7a6a2c13b0822f87ecf82
MW <N/A>
2016-10-08 12:10:18 +0200

    add audio clip loader

---
+++ Assets/Tasks/LoadAudioClipFromStream.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAudioClipFromStream : IAsyncTask
+{
+    public event Notification<LoadAudioClipFromStream, IAsset> Completed = null;
+
+    private WWW source = null;
+    private AssetInfo metaData = null;
+    private bool isCanceled = false;
+    private bool isDone = false;
+    public LoadAudioClipFromStream(WWW stream, AssetInfo assetInfo)
+    {
+        source = stream;
+        metaData = assetInfo;
+    }
+
+    public bool IsDone
+    {
+        get 
+        {
+            return isDone;
+        }
+    }
+
+    public bool IsCanceled
+    {
+        get
+        {
+            return isCanceled;
+        }
+    }
+
+    public IEnumerator Run()
+    {
+        if (source.assetBundle != null)
+        {
+            AssetBundleRequest request = source.assetBundle.LoadAssetAsync<AudioClip>(metaData.Name);
+            yield return request;
+            if (Completed != null)
+            {
+                IAsset asset = new CharacterAsset(metaData, (GameObject)request.asset);
+                Completed(this, asset);
+            }
+            isDone = request.isDone;
+        }
+        else
+        {
+            isCanceled = true;
+        }
+    }
+}
