b43cc2f1f64c80e52794516ce2201882b1e5cea3
MW <N/A>
2016-10-08 12:41:35 +0200

    add game manager stub

---
+++ Assets/Services/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset[] metaDataFiles = null;
+
+    private IMetaDataReader metaDataReader = null;
+    private IAssetLoader assetLoader = null;
+    private IAssetManager assetManager = null;
+    // Use this for initialization
+    void Start()
+    {
+        IAsyncService asyncService = GetComponent<IAsyncService>();
+        if(asyncService != null)
+        {
+            assetManager = new AssetManager(metaDataFiles.Length);
+            JSONReader jsonReader = new JSONReader(metaDataFiles);
+
+            WebLoader webLoader = new WebLoader(asyncService, metaDataReader);
+            metaDataReader.StartReading();
+        }
+        else
+        {
+            Debug.Log("Missing AsyncService");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
