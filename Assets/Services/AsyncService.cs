776a5bb856d5070ccc4dfadae5a032800fdb21a3
MW <N/A>
2016-10-08 10:46:22 +0200

    add simple async service

---
+++ Assets/Services/AsyncService.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple async service to run operations
+/// </summary>
+public class AsyncService : MonoBehaviour, IAsyncService
+{
+
+    public void RunTask(IAsyncTask task)
+    {
+        StartCoroutine(StartTask(task));
+    }
+
+    private IEnumerator StartTask(IAsyncTask task)
+    {
+        while(!task.IsDone || !task.IsCanceled)
+        {
+            yield return task.Run();
+        }
+    }
+}
