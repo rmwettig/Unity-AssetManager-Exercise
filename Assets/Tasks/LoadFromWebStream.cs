a790a343415033c22c7bb800bbb9e4861b5647ab
MW <N/A>
2016-10-08 11:06:54 +0200

    add web streaming task stub

---
+++ Assets/Tasks/LoadFromWebStream.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void Notification<S,T>(S sender, T result);
+
+public class LoadFromWebStream : IAsyncTask
+{
+    public event Notification<LoadFromWebStream, WWW> Completed = null;
+
+    private AssetInfo metaData = null;
+
+    public LoadFromWebStream(AssetInfo assetInfo)
+    {
+        metaData = assetInfo;
+    }
+
+    public bool IsDone
+    {
+        get { throw new System.NotImplementedException(); }
+    }
+
+    public bool IsCanceled
+    {
+        get { throw new System.NotImplementedException(); }
+    }
+
+    public IEnumerator Run()
+    {
+        WWW webStream = new WWW(metaData.URL);
+        yield return webStream;
+        if(Completed != null)
+        {
+            Completed(this, webStream);
+        }
+    }
+}
