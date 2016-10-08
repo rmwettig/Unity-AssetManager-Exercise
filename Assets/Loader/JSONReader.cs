a6f4e2402f461a51dc579a0407d781402ea96859
MW <N/A>
2016-10-08 10:53:46 +0200

    add json reader

---
+++ Assets/Loader/JSONReader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void Notification<T>(T result);
+
+public class JSONReader : IMetaDataReader
+{
+    public event Notification<AssetInfo> MetaDataLoaded = null;
+    private TextAsset[] files = null;
+
+    public JSONReader(params TextAsset[] jsonFiles)
+    {
+        files = jsonFiles;
+    }
+
+    public void StartReading()
+    {
+        for(int i = 0; i < files.Length; i++)
+        {
+            AssetInfo ai = JsonUtility.FromJson<AssetInfo>(files[i].text);
+            if(MetaDataLoaded != null)
+            {
+                MetaDataLoaded(ai);
+            }
+        }
+    }
+}
