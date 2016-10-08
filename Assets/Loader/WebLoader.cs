8c1a063908ebab2f6425fc97c82f2aad8ef5e445
MW <N/A>
2016-10-08 10:59:01 +0200

    add webloader stub

---
+++ Assets/Loader/WebLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebLoader : IAssetLoader
+{
+    private IAsyncService asyncService = null;
+
+    public WebLoader(IAsyncService service, IMetaDataReader reader)
+    {
+
+    }
+
+    public void StartLoading()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnMetaDataLoaded(AssetInfo assetInfo)
+    {
+
+    }
+}
