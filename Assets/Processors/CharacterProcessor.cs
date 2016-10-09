bdcc2587bd98b4d171eaa103f7c728cb44520bd8
MW <N/A>
2016-10-09 12:14:45 +0200

    add processor classes

---
+++ Assets/Processors/CharacterProcessor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterProcessor : WebStreamProcessor
+{
+
+    public CharacterProcessor(string type):base(type)
+    {
+
+    }
+
+    public override IAsyncTask CreateProcessingTask(WWW stream, AssetInfo metaData)
+    {
+        return new LoadCharacterFromStream(stream, metaData);
+    }
+}
---
+++ Assets/Processors/WebStreamProcessor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class WebStreamProcessor
+{
+    private string type = null;
+
+    public WebStreamProcessor(string targetType)
+    {
+        type = targetType.ToLower();
+    }
+
+    public bool CanProcessType(string otherType)
+    {
+        return type.Equals(otherType.ToLower());
+    }
+
+    public abstract IAsyncTask CreateProcessingTask(WWW stream, AssetInfo metaData);
+}
