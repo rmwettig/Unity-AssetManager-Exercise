d4ebfb204fe0467e4b91a6b4bb82bb0c0a852236
MW <N/A>
2016-10-09 12:53:44 +0200

    add audio processing and instantiating

---
+++ Assets/Processors/AudioProcessor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioProcessor : WebStreamProcessor
+{
+    public AudioProcessor(string type) : base(type) { }
+
+    public override IAsyncTask CreateProcessingTask(WWW stream, AssetInfo metaData, Notification<IAsset> resultCallback)
+    {
+        LoadAudioClipFromStream task = new LoadAudioClipFromStream(stream, metaData);
+        task.Completed += resultCallback;
+        return task;
+    }
+}
---
+++ Assets/Tasks/InstantiateAudioClip.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateAudioClip : IAsyncTask
+{
+
+    private IAssetManager assetManager = null;
+    private string assetName = null;
+    private bool isDone = false;
+    private bool isCanceled = false;
+    public InstantiateAudioClip(IAssetManager manager, string name)
+    {
+        assetManager = manager;
+        assetName = name;
+    }
+
+    public bool IsDone
+    {
+        get
+        {
+            return isDone;
+        }
+    }
+
+    public bool IsCanceled
+    {
+        get 
+        {
+            return isCanceled;
+        }
+    }
+
+    public IEnumerator Run()
+    {
+        IAsset audioAsset = assetManager.FindAssetByName(assetName);
+        while(audioAsset == null)
+        {
+            audioAsset = assetManager.FindAssetByName(assetName);
+            yield return null;
+        }
+        AudioClip clip = audioAsset.TryGetAsType<AudioClip>();
+        if (clip != null)
+        {
+            GameObject go = new GameObject(assetName);
+            AudioSource audio = go.AddComponent<AudioSource>();
+            audio.clip = clip;
+            audio.Play();
+            isDone = true;
+        }
+        else
+        {
+            Debug.Log("no audio clip found");
+        }
+    }
+}
