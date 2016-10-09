c55269412893e9fdda53120c82885fd4828428d3
MW <N/A>
2016-10-09 13:31:45 +0200

    add match strategy

---
+++ Assets/Matcher/MatchAssetName.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Matches the exact name of an asset
+/// </summary>
+public class MatchAssetName : IAssetMatcher
+{
+    private string name = null;
+
+    public MatchAssetName(string assetName)
+    {
+        name = assetName;
+    }
+
+    public bool MatchesCriterion(IAsset asset)
+    {
+        return name.Equals(asset.AssetInfo.AssetName);
+    }
+}
